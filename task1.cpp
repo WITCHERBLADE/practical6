c66bbd5b9d1f37387e221afac17a88a0305bd7a0
WITCHERBLADE <87032590+WITCHERBLADE@users.noreply.github.com>
2021-10-06 10:38:52 +0530

    Create task1.cpp

---
+++ task1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+int a=23 , b =21;
+float a1 =12.45 , b1 = 45.56;
+string a2 ="hello" , b2 = "there";
+
+
+cout<<"the sum for integer is "<<(a+b)<<endl;
+cout<<"the sum for float is "<<(a1+b1)<<endl;
+cout<<"the sum for string is "<<(a2+b2)<<endl;
+cout<<"the sum for string and integer is "<<(a2+b)<<endl;
+return 0;
+}
