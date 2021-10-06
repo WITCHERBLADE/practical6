074539f51d2f692f18d47f1185e00d2205e95e9e
WITCHERBLADE <87032590+WITCHERBLADE@users.noreply.github.com>
2021-10-06 10:40:20 +0530

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+string str="";
+cout<<"enter  the  string : ";
+cin>>str;
+reverse(str.begin(),str.end());
+cout<<str<<endl;
+return 0;
+}
+
+ 
