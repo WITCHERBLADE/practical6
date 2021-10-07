401137a301ebfe2a161fc2a47711488aa5de98e7
WITCHERBLADE <87032590+WITCHERBLADE@users.noreply.github.com>
2021-10-07 10:41:44 +0530

    Create task4.cpp

---
+++ task4.cpp
@@ -0,0 +1,16 @@
+//DIFFERENCE BETWEEN THE ASCII VALUE OF CHARACTERS
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+
+char a , b;
+
+cout<<"ENTER THE CHAR A :"<<endl;
+cin>>a;
+cout<<"ENTER THE CHAR B :"<<endl;
+cin>>b;
+cout<<int(a-b);
+return 0;
+}
